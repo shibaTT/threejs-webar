21794117a2eb28ad0e743bff7ae0a5fda0f275ee
ShibaTTTTTTTTT <39818834+shibaTT@users.noreply.github.com>
2024-03-01 17:02:41 +0900

    fix: some fixes.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("canvas") });
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const scene = new THREE.Scene();
+const markerGroup = new THREE.Group();
 const arCtx = THREEx.ArToolkitContext({
     cameraParametersUrl: "../data/camera.dat",
     detectionMode: "mono",
@@ -8,7 +9,7 @@ const arCtx = THREEx.ArToolkitContext({
 const arSource = THREEx.ArToolkitSource({
     sourceType: "webcam",
 });
-const arMarker = new THREEx.ArMarkerControls(arCtx, camera, {
+const arMarker = new THREEx.ArMarkerControls(arCtx, markerGroup, {
     type: "pattern",
     patternUrl: "../data/pattern-hiro.patt",
 });
@@ -25,7 +26,8 @@ arCtx.init(() => {
     camera.projectionMatrix.copy(arCtx.getProjectionMatrix());
 });
 arSource.init(() => {
-    document.body.appendChild(arSource.domElement);
+    console.log("今から配下に追加します");
+    document.querySelector("main").appendChild(arSource.domElement);
     arSource.onResizeElement();
     arSource.copyElementSizeTo(renderer.domElement);
     if (arCtx.arController) {
@@ -33,7 +35,7 @@ arSource.init(() => {
     }
 });
 
-scene.add(arSource.domElement);
+scene.add(markerGroup);
 
 const cube = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
@@ -42,12 +44,10 @@ const cube = new THREE.Mesh(
 
 cube.position.set(0, 0.5, 0);
 
-scene.add(cube);
+markerGroup.add(cube);
 
 renderer.setAnimationLoop(() => {
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 });
-
-renderer.setPixelRatio(window.devicePixelRatio);
